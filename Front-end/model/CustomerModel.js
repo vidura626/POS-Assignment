93077cc1953a749d52d238a47f6ab61ae5fcb5e9
vidura626 <wijesinghemap@gmail.com>
2023-07-28 23:48:51 +0530

    Initial commit

---
+++ Front-end/model/CustomerModel.js
@@ -0,0 +1,8 @@
+class CustomerModel{
+    constructor(custID,custName,custAddress,custSalary) {
+        this.custID=custId;
+        this.custName=custName;
+        this.custAddress=custAddress;
+        this.custSalary=custSalary;
+    }
+}
