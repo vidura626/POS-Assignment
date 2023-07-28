80d1ce39aee15f3c886eb98eb581e439523430de
vidura626 <wijesinghemap@gmail.com>
2023-07-29 02:54:26 +0530

    02 structure modified

--- Front-end/controller/customerController.js
+++ Front-end/controller/customerController.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
 
+//    Validation
+
+//    Add
+    $(".js-btn-add-customer").click(function () {
+        console.log("clicked");
+
+        let custId = $("#customer-id").val();
+        let custName = $("#customer-name").val();
+        let custAddress = $("#customer-address").val();
+        let custSalary = $("#customer-salary").val();
+
+
+        let customerModel = new CustomerModel(custId, custName, custAddress, custSalary);
+        console.log(customerModel)
+
+        $.ajax({
+            url: "http://localhost:8080/pos/customer",
+            contentType: "application/json",
+            data: JSON.stringify(customerModel),
+            method: 'POST',
+            success: function (resp) {
+                if (resp.status === 200) {
+                    alert("Customer Added Successfully");
+                } else {
+                    alert("Customer Added Failed".concat(resp.message));
+                }
+            },
+            error: function (err) {
+
+            }
+        });
+        // console.log(id,name,address,salary)
+
+    });
+//    Update
+
+//    Delete
+
+//    Search
+
 })
--- Front-end/model/CustomerModel.js
+++ Front-end/model/CustomerModel.js
@@ -1,6 +1,6 @@
 class CustomerModel{
     constructor(custID,custName,custAddress,custSalary) {
-        this.custID=custId;
+        this.custID=custID;
         this.custName=custName;
         this.custAddress=custAddress;
         this.custSalary=custSalary;
