2524bee797b4bf45d11ba31d89d1ea5df3bf1afd
vidura626 <wijesinghemap@gmail.com>
2023-07-30 00:23:33 +0530

    tests3

--- Back-end/src/main/java/lk/ijse/Back_end/controller/CustomerServlet.java
+++ Back-end/src/main/java/lk/ijse/Back_end/controller/CustomerServlet.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 @WebServlet("/customer")
 public class CustomerServlet extends HttpServlet {
-
+int y = 100;
     Jsonb jsonb = JsonbBuilder.create();
 
     CustomerBO customerBO = new CustomerBOImple();
