2a58e768bf85c754341cd4846b435ad61ba00976
vidura626 <wijesinghemap@gmail.com>
2023-07-29 21:01:00 +0530

    Customer add update delete methods coded

--- Front-end/controller/customerController.js
+++ Front-end/controller/customerController.js
@@ -1,43 +1,99 @@
 $(document).ready(function () {
+  //    Validation
 
-//    Validation
+  //    Add
+  $(".js-btn-add-customer").click(function () {
+    console.log("add clicked");
 
-//    Add
-    $(".js-btn-add-customer").click(function () {
-        console.log("clicked");
+    let custId = $("#customer-id").val();
+    let custName = $("#customer-name").val();
+    let custAddress = $("#customer-address").val();
+    let custSalary = $("#customer-salary").val();
 
-        let custId = $("#customer-id").val();
-        let custName = $("#customer-name").val();
-        let custAddress = $("#customer-address").val();
-        let custSalary = $("#customer-salary").val();
+    let customerModel = new CustomerModel(
+      custId,
+      custName,
+      custAddress,
+      custSalary
+    );
+    console.log(customerModel);
 
-
-        let customerModel = new CustomerModel(custId, custName, custAddress, custSalary);
-        console.log(customerModel)
-
-        $.ajax({
-            url: "http://localhost:8080/pos/customer",
-            contentType: "application/json",
-            data: JSON.stringify(customerModel),
-            method: 'POST',
-            success: function (resp) {
-                if (resp.status === 200) {
-                    alert("Customer Added Successfully");
-                } else {
-                    alert("Customer Added Failed".concat(resp.message));
-                }
-            },
-            error: function (err) {
-
-            }
-        });
-        // console.log(id,name,address,salary)
-
+    $.ajax({
+      url: "http://localhost:8080/Back_end_war/customer",
+      contentType: "application/json",
+      data: JSON.stringify(customerModel),
+      method: "POST",
+      success: function (resp) {
+        console.log(resp);
+        if (resp.code === 200) {
+          alert("Customer Added Successfully");
+        } else {
+          alert("Customer Added Failed : ".concat(resp.message));
+        }
+      },
+      error: function (err) {
+        console.log(err);
+      },
     });
-//    Update
+    // console.log(id,name,address,salary)
+  });
+  //    Update
+  $(".js-btn-update-customer").click(function () {
+    console.log("update clicked");
 
-//    Delete
+    let custId = $("#customer-id").val();
+    let custName = $("#customer-name").val();
+    let custAddress = $("#customer-address").val();
+    let custSalary = $("#customer-salary").val();
 
-//    Search
+    let customerModel = new CustomerModel(
+      custId,
+      custName,
+      custAddress,
+      custSalary
+    );
+    console.log(customerModel);
 
-})
+    $.ajax({
+      url: "http://localhost:8080/Back_end_war/customer",
+      contentType: "application/json",
+      data: JSON.stringify(customerModel),
+      method: "PUT",
+      success: function (resp) {
+        console.log(resp);
+        if (resp.code === 200) {
+          alert("Customer Updated Successfully");
+        } else {
+          alert("Customer Updated Failed : ".concat(resp.message));
+        }
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+    // console.log(id,name,address,salary)
+  });
+  //    Delete
+  $(".js-btn-delete-customer").click(function () {
+    console.log("delete clicked");
+    let custId = $("#customer-id").val();
+
+    $.ajax({
+      url: "http://localhost:8080/Back_end_war/customer?custId=".concat(custId),
+      contentType: "application/json",
+      method: "DELETE",
+      success: function (resp) {
+        console.log(resp);
+        if (resp.code === 200) {
+          alert("Customer Deleted Successfully");
+        } else {
+          alert("Customer Delete Failed : ".concat(resp.message));
+        }
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  });
+  //    Search
+});
