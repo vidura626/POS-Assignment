fda1b5c217a5f7eafb931b1e1e0fc188d9212cc1
vidura626 <wijesinghemap@gmail.com>
2023-07-30 22:08:12 +0530

    Navigation coded.

--- Front-end/util/navBar.js
+++ Front-end/util/navBar.js
@@ -1,48 +1,11 @@
+navigate($("#sec-customer"));
 
-$('#btnDash').click(function (){
-    $('#dashboardPage').css('display','block');
-    $('#orderPage').css('display','none');
-    $('#itemPage').css('display','none');
-    $('#customerPage').css('display','none');
+$("#btnCustomerNav").click(() => navigate($("#sec-customer")));
+$("#btnItemNav").click(() => navigate($("#sec-item")));
+// $("#btnOrderNav").click(() => navigate($(".sec-placeOrder")));
 
-    $('#btnDash').css({"font-weight": "bold"});
-    $('#btnOrder').css({"font-weight": "normal"});
-    $('#btnItem').css({"font-weight": "normal"});
-    $('#btnCustomer').css({"font-weight": "normal"});
-});
-
-$('#btnOrder').click(function (){
-    $('#dashboardPage').css('display','none');
-    $('#orderPage').css('display','block');
-    $('#itemPage').css('display','none');
-    $('#customerPage').css('display','none');
-
-    $('#btnDash').css({"font-weight": "normal"});
-    $('#btnOrder').css({"font-weight": "bold"});
-    $('#btnItem').css({"font-weight": "normal"});
-    $('#btnCustomer').css({"font-weight": "normal"});
-});
-
-$('#btnItem').click(function (){
-    $('#dashboardPage').css('display','none');
-    $('#orderPage').css('display','none');
-    $('#itemPage').css('display','block');
-    $('#customerPage').css('display','none');
-
-    $('#btnDash').css({"font-weight": "normal"});
-    $('#btnOrder').css({"font-weight": "normal"});
-    $('#btnItem').css({"font-weight": "bold"});
-    $('#btnCustomer').css({"font-weight": "normal"});
-});
-
-$('#btnCustomer').click(function (){
-    $('#dashboardPage').css('display','none');
-    $('#orderPage').css('display','none');
-    $('#itemPage').css('display','none');
-    $('#customerPage').css('display','block');
-
-    $('#btnDash').css({"font-weight": "normal"});
-    $('#btnOrder').css({"font-weight": "normal"});
-    $('#btnItem').css({"font-weight": "normal"});
-    $('#btnCustomer').css({"font-weight": "bold"});
-});
+function navigate(location) {
+    var sections = $("main>section");
+    for (let i = 0; i < sections.length; i++) sections.eq(i).css('display', 'none');
+    location.css('display', 'block');
+}
