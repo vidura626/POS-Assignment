9eb689ff2c844dd3310401223f0966312d53b777
vidura626 <wijesinghemap@gmail.com>
2023-07-30 20:08:37 +0530

    Item Model Created in front end.

---
+++ Front-end/model/ItemModel.js
@@ -0,0 +1,8 @@
+class ItemModel{
+    constructor(itemCode,itemName,itemQty,itemUnitPrice) {
+        this.itemCode=itemCode;
+        this.itemName=itemName;
+        this.itemQty=itemQty;
+        this.itemUnitPrice=itemUnitPrice;
+    }
+}
