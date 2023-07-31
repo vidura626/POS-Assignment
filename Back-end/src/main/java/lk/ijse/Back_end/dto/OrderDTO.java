5ce85c02515eb4a0ea3c7a29e31f0bd2b8bd28af
vidura626 <wijesinghemap@gmail.com>
2023-07-31 18:32:32 +0530

    Changed properties to get front end data.

--- Back-end/src/main/java/lk/ijse/Back_end/dto/OrderDTO.java
+++ Back-end/src/main/java/lk/ijse/Back_end/dto/OrderDTO.java
@@ -1,8 +1,11 @@
 package lk.ijse.Back_end.dto;
 
+import jakarta.json.bind.annotation.JsonbDateFormat;
 import lombok.*;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,7 +15,17 @@ import java.sql.Date;
 public class OrderDTO {
     private String orId;
     private String orCusId;
-    private Date orDate;
+    @JsonbDateFormat(value = "yyyy-MM-dd")
+    private java.util.Date orDate;
     private double orDis;
     private double orSubTotal;
+    private List<OrderDetailsDTO> orderDetails;
+
+    public OrderDTO(String orId, String orCusId, Date orDate, double orDis, double orSubTotal) {
+        this.orId = orId;
+        this.orCusId = orCusId;
+        this.orDate = orDate;
+        this.orDis = orDis;
+        this.orSubTotal = orSubTotal;
+    }
 }
